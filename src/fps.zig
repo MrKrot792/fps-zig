7a76898dcf9a319f6529ee502670f529a3d90a96
Vladislav Korotianets <vkorotanec@gmail.com>
2025-12-27 01:47:20 +0100

    initial commit

---
+++ src/fps.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+
+const Fps = @This();
+pub const Timer = std.time.Timer;
+var frames_count: u32 = 0;
+var frames: u32 = 0;
+var elapsed: f32 = 0;
+
+pub const FpsInfo = struct { 
+    fps: u32, 
+    fps_average: f32, 
+    delta: f32,
+
+    /// pretty useful ig
+    pub const zero = FpsInfo{
+        .fps = 0,
+        .fps_average = 0,
+        .delta = 0,
+    };
+};
+
+/// It is not neccesary to call this function every frame, although it is recommended.
+pub fn frameStart(timer: *Timer) void {
+    timer.reset(); // Yeah cuz why not
+}
+
+/// Make sure that this is called at the very end of your loop!
+pub fn frameEnd(timer: *Timer) FpsInfo {
+    const ns_per_frame = @as(f32, @floatFromInt(timer.lap()));
+    const d = ns_per_frame / 1.0e9;
+
+    elapsed += d;
+    frames_count += 1;
+
+    if (elapsed >= 1.0) {
+        elapsed = 0;
+        frames = frames_count;
+        frames_count = 0;
+    }
+
+    return .{
+        .delta = d,
+        .fps = frames,
+        .fps_average = 1.0 / d,
+    };
+}
+
